6a10149b8f53ecbd805d752377345319f3739874
alexguirre <alexguirre.aa@gmail.com>
2019-10-31 00:55:16 +0100

    Cast to integer type before printf

--- src/file-explorer/util/Format.cpp
+++ src/file-explorer/util/Format.cpp
@@ -8,10 +8,10 @@ std::string BytesToHumanReadable(std::size_t numBytes)
 	static const std::array Units{ "bytes", "KB", "MB", "GB", "TB", "PB", "EB", "ZB", "YB" };
 
 	const std::size_t i = static_cast<std::size_t>(std::floor(std::log(numBytes) / std::log(1024)));
-	const double convertedBytes = (numBytes / std::pow(1024.0, i));
+	const std::size_t bytesInUnits = static_cast<std::size_t>(numBytes / std::pow(1024.0, i));
 
 	std::array<char, 256> buffer{};
-	std::snprintf(buffer.data(), buffer.size(), "%.0f %s", convertedBytes, Units[i]);
+	std::snprintf(buffer.data(), buffer.size(), "%u %s", bytesInUnits, Units[i]);
 
 	return { buffer.data() };
 }
