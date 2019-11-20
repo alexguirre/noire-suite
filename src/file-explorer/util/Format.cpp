e5dee4d975812a09f9c67d11b745c52727584a58
alexguirre <alexguirre.aa@gmail.com>
2019-11-20 01:13:52 +0100

    Handle special cases in BytesToHumanReadable
    When numBytes == 0, it crashed Release builds, numBytes == 1 needs singular form.

--- src/file-explorer/util/Format.cpp
+++ src/file-explorer/util/Format.cpp
@@ -1,17 +1,35 @@
 #include "Format.h"
 #include <array>
 #include <cmath>
+#include <gsl/gsl>
 
 std::string BytesToHumanReadable(std::uintmax_t numBytes)
 {
-	// https://ourcodeworld.com/articles/read/713/converting-bytes-to-human-readable-values-kb-mb-gb-tb-pb-eb-zb-yb-with-javascript
-	static const std::array Units{ "bytes", "KB", "MB", "GB", "TB", "PB", "EB", "ZB", "YB" };
+	// special cases
+	if (numBytes == 0)
+	{
+		// default case gets i == -1 if numBytes == 0, causing to go out-of-bounds when reading
+		// Units array
+		return "0 bytes";
+	}
+	else if (numBytes == 1)
+	{
+		// needs singular form
+		return "1 byte";
+	}
+	else // default case
+	{
+		// https://ourcodeworld.com/articles/read/713/converting-bytes-to-human-readable-values-kb-mb-gb-tb-pb-eb-zb-yb-with-javascript
+		static const std::array Units{ "bytes", "KB", "MB", "GB", "TB", "PB", "EB", "ZB", "YB" };
 
-	const std::size_t i = static_cast<std::size_t>(std::floor(std::log(numBytes) / std::log(1024)));
-	const std::uintmax_t bytesInUnits = static_cast<std::uintmax_t>(numBytes / std::pow(1024.0, i));
+		const std::size_t i =
+			static_cast<std::size_t>(std::floor(std::log(numBytes) / std::log(1024)));
+		const std::uintmax_t bytesInUnits =
+			static_cast<std::uintmax_t>(numBytes / std::pow(1024.0, i));
 
-	std::array<char, 256> buffer{};
-	std::snprintf(buffer.data(), buffer.size(), "%ju %s", bytesInUnits, Units[i]);
+		std::array<char, 256> buffer{};
+		std::snprintf(buffer.data(), buffer.size(), "%ju %s", bytesInUnits, Units[i]);
 
-	return { buffer.data() };
+		return { buffer.data() };
+	}
 }
