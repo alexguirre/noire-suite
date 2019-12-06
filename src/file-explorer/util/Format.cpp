470ba4b93e0356c3200446abc4e64e22fe79edb4
alexguirre <alexguirre.aa@gmail.com>
2019-12-06 01:51:02 +0100

    Make BytesToHumanReadable more similar to how Windows' File Explorer shows the size of files

--- src/file-explorer/util/Format.cpp
+++ src/file-explorer/util/Format.cpp
@@ -5,31 +5,22 @@
 
 std::string BytesToHumanReadable(std::uintmax_t numBytes)
 {
-	// special cases
-	if (numBytes == 0)
-	{
-		// default case gets i == -1 if numBytes == 0, causing to go out-of-bounds when reading
-		// Units array
-		return "0 bytes";
-	}
-	else if (numBytes == 1)
-	{
-		// needs singular form
-		return "1 byte";
-	}
-	else // default case
-	{
-		// https://ourcodeworld.com/articles/read/713/converting-bytes-to-human-readable-values-kb-mb-gb-tb-pb-eb-zb-yb-with-javascript
-		static const std::array Units{ "bytes", "KB", "MB", "GB", "TB", "PB", "EB", "ZB", "YB" };
+	// same as how Windows' File Explorer calculates the file size in KB
+	const std::uintmax_t kiloBytes = gsl::narrow_cast<std::uintmax_t>(std::ceil(numBytes / 1024.0));
 
-		const std::size_t i =
-			static_cast<std::size_t>(std::floor(std::log(numBytes) / std::log(1024)));
-		const std::uintmax_t bytesInUnits =
-			static_cast<std::uintmax_t>(numBytes / std::pow(1024.0, i));
+	std::array<char, 256> buffer{};
+	const int len = std::snprintf(buffer.data(), buffer.size(), "%ju KB", kiloBytes);
+	Expects(len >= 0);
 
-		std::array<char, 256> buffer{};
-		std::snprintf(buffer.data(), buffer.size(), "%ju %s", bytesInUnits, Units[i]);
-
-		return { buffer.data() };
+	std::string str{ buffer.data(), gsl::narrow_cast<std::size_t>(len) };
+	if (str.size() > 6)
+	{
+		// insert thousands separator
+		for (std::ptrdiff_t i = str.size() - 6; i >= 0; i -= 4)
+		{
+			str.insert(i, 1, ',');
+		}
 	}
+
+	return str;
 }
