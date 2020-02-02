cc0760062ee6682e25cf33b3369fe0c6a73f238d
alexguirre <alexguirre.aa@gmail.com>
2020-02-02 03:56:16 +0100

    Include missing SimpleMath.cpp
    https://github.com/microsoft/DirectXTK/blob/69420bd61191a84719ed8f5ffa943b65fec86e2f/Src/SimpleMath.cpp

--- src/file-explorer/rendering/Math.h
+++ src/file-explorer/rendering/Math.h
@@ -1,5 +1,8 @@
 #pragma once
+// NOTE: d3d11.h must be included before SimpleMath.h
+// clang-format off
 #include <d3d11.h>
 #include "SimpleMath.h"
+// clang-format on
 
 using namespace DirectX::SimpleMath;
